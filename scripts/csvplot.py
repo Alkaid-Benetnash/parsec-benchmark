bcaad64af0c1e28226042c71beb6dd0a72cef795
Alkaid <zgf574564920@gmail.com>
2022-12-19 16:35:16 -0800

    [csvplot] Plot field description in the final figure and tweak figsize

--- scripts/csvplot.py
+++ scripts/csvplot.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import argparse
 import pandas
 from pathlib import Path
 from itertools import cycle
+from csvFields import ALLCSVFIELDS, AllCSVFieldsIndexedByKey
+import textwrap as tw
 
 
 def buildParser():
@@ -12,7 +15,7 @@ def buildParser():
     parser.add_argument('--input', '-i', type=str, required=True,
                         help="The input csv file, the filename prefix (excluding the suffix .csv) will be used as the name of the experiment")
     parser.add_argument('--fields', '-F', type=lambda s: s.split(','), default="elapsed",
-                        help="Comma separated list of fields in the csv that we want to plot (default: %(default)s)")
+                        help=f"Comma separated list of fields in the csv that we want to plot (default: %(default)s) (all fields: {','.join([f.key for f in ALLCSVFIELDS])})")
     parser.add_argument('--drop-first', action="store_true",
                         help="Remove the first experiment (row) in the dataset, if you want to consider it as warmup.")
     parser.add_argument('--dir', '-d', type=str, default=".",
@@ -35,8 +38,10 @@ def plot(args):
     fig, axs = plt.subplots(
         len(args.fields), 1,
         squeeze=False,
-        figsize=(FIGWIDTH, FIGHEIGHT*len(args.fields))
+        figsize=(FIGWIDTH, FIGHEIGHT*len(args.fields)),
+        sharex="row",
     )
+    fig.subplots_adjust(hspace=0.4)
     csvData = pandas.read_csv(args.input)
     if args.drop_first:
         csvData.drop(0)
@@ -60,15 +65,18 @@ def plot(args):
             for x, y, err in zip(xaxis, yaxis, errorbars):
                 ax.errorbar(x, y, err, capsize=2, color=color)
             # draw subplot metadata
-            ax.set_title(f"{exprName}: ncores <-> {field}")
+            ax.set_title(f"ncores <-> {field}")
             ax.set_xlabel("ncores")
             ax.set_ylabel(f"{field} ({DEFAULT_FIELD_UNITS.get(field, '')})")
             ax.legend()
+            csvField = AllCSVFieldsIndexedByKey[field]
+            ax.annotate(tw.fill(f"{csvField.key}: {csvField.description}", width=100), (0, 0), (0, -40), xycoords="axes fraction", textcoords="offset points", va="top", wrap=True)
             allNcores = list(ncoresGroups.groups.keys())
             ax.set_xticks(allNcores, labels=allNcores)
     outdir = Path(args.dir)
     outdir.mkdir(exist_ok=True)
-    fig.savefig(outdir / f"{exprName}.plot.png", dpi=300, format="png")
+    fig.suptitle(f"{exprName}", fontsize="xx-large")
+    fig.savefig(outdir / f"{exprName}.plot.png", dpi=300, format="png", bbox_inches="tight")
     # plt.show()
 
 
